2b1761326dc2fac16d95a0bab09b242005504b25
Francisco Zambrano <franciscozambrano@outlook.es>
2020-08-09 19:05:35 -0500

    form register without function

---
+++ src/components/Modal/BasicModal/BasicModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Modal } from "react-bootstrap";
+import LogoWhiteTwittor from "../../../assets/img/png/logo-white.png";
+import "./BasicModal.scss";
+
+export default function BasicModal(props) {
+
+    const { show, setShow, children } = props;
+
+    return (
+        <Modal
+            className="basic-modal"
+            show={show}
+            onHide={() => setShow(false)}
+            centered
+            size="lg"
+        >
+            <Modal.Header>
+                <Modal.Title>
+                    <img src={LogoWhiteTwittor} alt="Twittor" />
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {children}
+            </Modal.Body>
+        </Modal>
+    )
+}
--- src/page/SignInSignUp/SignInSignUp.js
+++ src/page/SignInSignUp/SignInSignUp.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUsers, faComment } from "@fortawesome/free-solid-svg-icons";
+import "./SignInSignUp.scss"; import BasicModal from '../../components/Modal/BasicModal/BasicModal';
 import LogoTwittor from "../../assets/img/png/logo.png";
 import LogoWhiteTwittor from "../../assets/img/png/logo-white.png";
-import "./SignInSignUp.scss";;
+import SignUpForm from '../../components/SignUpForm';
+;
 
 export default function SignInSignUp() {
+
+    const [showModal, setShowModal] = useState(false);
+    const [contentModal, setContentModal] = useState(null);
+
+    const openModal = content => {
+        setShowModal(true);
+        setContentModal(content);
+    }
+
     return (
-        <Container className="signin-signup" fluid>
-            <Row>
-                <LeftComponent />
-                <RightComponent />
-            </Row>
-        </Container>
+        <>
+            <Container className="signin-signup" fluid>
+                <Row>
+                    <LeftComponent />
+                    <RightComponent
+                        openModal={openModal}
+                        setShowModal={setShowModal}
+                    />
+                </Row>
+            </Container>
+            <BasicModal
+                show={showModal}
+                setShow={setShowModal}
+            >
+                {contentModal}
+            </BasicModal>
+        </>
     );
 }
 
@@ -20,18 +44,44 @@ function LeftComponent() {
         <Col className="signin-signup__left" xs={6}>
             <img src={LogoTwittor} alt="Twittor" />
             <div>
-                <h2>- Sigue lo que te interesa.</h2>
-                <h2>- Entérate de qué está hablando la gente.</h2>
-                <h2>- Únete a la conversación.</h2>
+                <h2>
+                    <FontAwesomeIcon icon={faSearch} />
+                    Sigue lo que te interesa.
+                </h2>
+                <h2>
+                    <FontAwesomeIcon icon={faUsers} />
+                    Entérate de qué está hablando la gente.
+                </h2>
+                <h2>
+                    <FontAwesomeIcon icon={faComment} />
+                    Únete a la conversación.
+                </h2>
             </div>
         </Col>
     );
 }
 
-function RightComponent() {
+function RightComponent(props) {
+    const { openModal, setShowModal } = props;
     return (
         <Col className="signin-signup__right" xs={6}>
-            <h2>Right Component...</h2>
+            <div>
+                <img src={LogoWhiteTwittor} alt="Twittor" />
+                <h2>Mira lo que está pasando en el mundo en este momento.</h2>
+                <h3>Únete a Twitter hoy mismo.</h3>
+                <Button
+                    variant="primary"
+                    onClick={() => openModal(<SignUpForm setShowModal={setShowModal} />)}
+                >
+                    Regístrate
+                </Button>
+                <Button
+                    variant="outline-primary"
+                    onClick={() => openModal(<h2>Formulario de Login</h2>)}
+                >
+                    Iniciar sesión
+                </Button>
+            </div>
         </Col>
     );
 }
